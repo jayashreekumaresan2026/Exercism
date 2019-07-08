dd364d7a11b953db927de2c47ea3ab16126799b2
Jayashree <jayashree.kammu@gmail.com>
2019-07-08 12:39:23 +0530

    update RNA-transcription solution

--- rna-transcription/src/main/java/RnaTranscription.java
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -1,44 +1,22 @@
+import java.util.HashMap;
+
 class RnaTranscription {
+    HashMap<String, String> map = new HashMap<>();
+
+    {
+        map.put("G", "C");
+        map.put("C", "G");
+        map.put("T", "A");
+        map.put("A", "U");
+    }
+
     String transcribe(String dnaStrand) {
-        String result = "";
-        if (dnaStrand.length() == 0) {
-            result = "";
-        } else if (dnaStrand.length() == 1) {
-            char c = dnaStrand.charAt(0);
-            result = checkCharacter(c);
-        } else if (dnaStrand.length() > 2) {
-            result = splitTheString(dnaStrand);
+            StringBuilder stringBuilder = new StringBuilder();
+            char[] array = dnaStrand.toCharArray();
+            for (int i = 0; i < array.length; i++) {
+                stringBuilder.append(map.get(String.valueOf(array[i])));
+            }
+            return stringBuilder.toString();
         }
-        return result;
-    }
-
-    private String splitTheString(String dnaStrand) {
-        String result = "";
-        char[] array = dnaStrand.toCharArray();
-        for (int i = 0; i < array.length; i++) {
-            result += checkCharacter(array[i]);
-        }
-        return result;
-    }
-
-    String checkCharacter(char dnaStrand) {
-        String result = "";
-        switch (dnaStrand) {
-            case 'G':
-                result = "C";
-                break;
-            case 'C':
-                result = "G";
-                break;
-            case 'A':
-                result = "U";
-                break;
-            case 'T':
-            case 'U':
-                result = "A";
-                break;
-        }
-        return result;
-    }
 }
 
